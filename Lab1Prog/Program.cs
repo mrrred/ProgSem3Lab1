323580f0f3a21edd359677c85d04240eafdc7ce2
mrrred <zarubinr85@gmail.com>
2025-10-05 13:16:54 +0700

    MenuFix

--- Lab1Prog/Program.cs
+++ Lab1Prog/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Lab1Prog.Technique;
 using Lab1Prog.Technique.Keyboards;
 using Lab1Prog.Technique.Monitors;
+using Microsoft.VisualBasic;
 using System;
 using System.Collections.Generic;
 
@@ -8,15 +9,40 @@ namespace Lab1Prog.TestProgram
 {
     public class Program
     {
+        static public void InputComputeTechInfo(ref string? manufacturer, ref int year, ref int storageCapacity, ref double price)
+        {
+            Console.WriteLine("Введите производителя: ");
+            manufacturer = Console.ReadLine();
+            Console.WriteLine("Введите год: ");
+            year = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите объем: ");
+            storageCapacity = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите цену: ");
+            price = Convert.ToDouble(Console.ReadLine());
+        }
+
+        static public void InputMonitorInfo(ref double screenSize, ref int refreshRate)
+        {
+            Console.WriteLine("Введите размер экрана (в дюймах): "); ;
+            screenSize = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите частоту обновления (в Гц): "); ;
+            refreshRate = Convert.ToInt32(Console.ReadLine());
+        }
+
+        static public void InputKeyboardInfo(ref string? layout)
+        {
+            Console.WriteLine("Введите раскладку клавиатуры: "); ;
+            layout = Console.ReadLine();
+        }
+
         static public void Main(string[] args)
         {
             var devicesList = new List<ComputerTech>();
 
             char choice;
-            string? manufacturer;
-            int year, val;
-            double price;
-            string? layout; // перемещаем объявление переменной сюда
+            string? manufacturer = "";
+            int year = 0, val = 0;
+            double price = 1;
 
             do
             {
@@ -29,45 +55,21 @@ namespace Lab1Prog.TestProgram
                 switch (choice)
                 {
                     case 'T':
-                        Console.WriteLine("Введите производителя: ");
-                        manufacturer = Console.ReadLine();
-                        Console.WriteLine("Введите год: ");
-                        year = Convert.ToInt32(Console.ReadLine());
-                        Console.WriteLine("Введите объем: ");
-                        val = Convert.ToInt32(Console.ReadLine());
-                        Console.WriteLine("Введите цену: ");
-                        price = Convert.ToDouble(Console.ReadLine());
+                        InputComputeTechInfo(ref manufacturer, ref year, ref val, ref price);
                         newDevice = new ComputerTech(manufacturer ?? "", year, val, price);
                         break;
                     case 'M':
-                        double screenSize;
-                        int refreshRate;
-                        Console.WriteLine("Введите производителя: ");
-                        manufacturer = Console.ReadLine();
-                        Console.WriteLine("Введите год: ");
-                        year = Convert.ToInt32(Console.ReadLine());
-                        Console.WriteLine("Введите объем: ");
-                        val = Convert.ToInt32(Console.ReadLine());
-                        Console.WriteLine("Введите цену: ");
-                        price = Convert.ToDouble(Console.ReadLine());
-                        Console.WriteLine("Введите размер экрана (в дюймах): "); ;
-                        screenSize = Convert.ToDouble(Console.ReadLine());
-                        Console.WriteLine("Введите частоту обновления (в Гц): "); ;
-                        refreshRate = Convert.ToInt32(Console.ReadLine());
+                        double screenSize = 1;
+                        int refreshRate = 1;
+                        InputComputeTechInfo(ref manufacturer, ref year, ref val, ref price);
+                        InputMonitorInfo(ref screenSize, ref refreshRate);
                         newDevice = new Display(manufacturer ?? "", year, val, price, screenSize, refreshRate);
                         break;
                     case 'K':
-                        Console.WriteLine("Введите производителя: ");
-                        manufacturer = Console.ReadLine();
-                        Console.WriteLine("Введите год: ");
-                        year = Convert.ToInt32(Console.ReadLine());
-                        Console.WriteLine("Введите объем: ");
-                        val = Convert.ToInt32(Console.ReadLine());
-                        Console.WriteLine("Введите цену: ");
-                        price = Convert.ToDouble(Console.ReadLine());
-                        Console.WriteLine("Введите раскладку клавиатуры: "); ;
-                        layout = Console.ReadLine();
-                        newDevice = new Keyboard(manufacturer ?? "", year, val, price, layout);
+                        string? layout = "";
+                        InputComputeTechInfo(ref manufacturer, ref year, ref val, ref price);
+                        InputKeyboardInfo(ref layout);
+                        newDevice = new Keyboard(manufacturer ?? "", year, val, price, layout ?? "QWERTY");
                         break;
                     case 'E':
                         break;
